fa6068e50621fd70d63320e94f360d37a12302a3
OFFICIAL INSPIRE <zach@inspireclothing.art>
2026-09-19 12:30:19 -0400

    Add Phase 16A startup gate and Inspire branding

---
+++ src/assets/brandAssets.js
@@ -0,0 +1,5 @@
+import inspireLogoUrl from '../../inspire.png?url';
+import inspireIntroUrl from '../../inspiresoftwareintro.mp4?url';
+
+export const INSPIRE_LOGO_KEY = 'inspire-logo';
+export { inspireLogoUrl, inspireIntroUrl };
